db1e48becfb39f11fed3de2ebf53f68bdc5137ce
hasaravut-ship-it <hasaravut@gmail.com>
2026-03-21 09:12:52 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function login() {
+  const user = document.getElementById("username").value;
+  const pass = document.getElementById("password").value;
+
+  // hardcoded (demo only)
+  if (user === "admin" && pass === "1234") {
+    // store login state
+    localStorage.setItem("auth", "true");
+
+    // redirect to main page
+    window.location.href = "index.html";
+  } else {
+    document.getElementById("error").innerText = "Invalid credentials";
+  }
+}
